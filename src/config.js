9c049d96596a8db5eddf21e422f0c9cb346eb76d
Ignacio Rodriguez Faillace <ignacio.rodriguez@santexgroup.com>
2017-10-20 17:13:17 -0300

    Switch to prod API

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://testresource-server.openstack.org'
+export const API_URL = 'https://openstackid-resources.openstack.org'
 export const API_VERSION = 'v1'
 
 export const TIME_URL = 'https://timeintervalsince1970.appspot.com'
