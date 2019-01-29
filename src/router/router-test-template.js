e7db38238e425e1c5e63ce7d5cb43112d4c51147
ClintE <cespin@gmail.com>
2019-01-29 15:25:04 -0600

    remove duplicate Screen from router-test-template.js

--- src/router/router-test-template.js
+++ src/router/router-test-template.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Screen from '../components/test-template/screen.vue'
-import Screen from '../components/ocp-ams/fnsign/screen.vue'
+// import Screen from '../components/ocp-ams/fnsign/screen.vue'
 
 Vue.use(VueRouter)
 
