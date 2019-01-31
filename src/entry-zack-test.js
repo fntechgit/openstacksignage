a2ff7e345c9705d54eb60d32ac08043156212da7
Zachary Muir <zack@fntech.com>
2019-01-30 18:37:56 -0800

    Fixed naming issue in entry-zack-test.js

--- src/entry-zack-test.js
+++ src/entry-zack-test.js
@@ -3,7 +3,7 @@ import App from './components/app.vue'
 
 import Schedule from './model/schedule'
 import { $store } from './store'
-import { $router } from './router/zack-test'
+import { $router } from './router/router-zack-test'
 
 require('./firebase')
 
