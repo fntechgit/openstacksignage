0e75511a85d406ecae655489cd35f2a1288b4fec
Diego Dindurra <diego.dindurra@gmail.com>
2019-02-06 17:49:06 +0000

    Creating a production Firebase project. Find the way of oving settings out of admin.html.

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export const API_URL = 'https://api.staging.fnopen.com'
-export const API_VERSION = 'v1'
-
-export const TIME_URL = 'https://timeintervalsince1970.appspot.com'
-
-export const FIREBASE_API_KEY = 'AIzaSyBPUYAZxU9DqLyUfZrSKpmIsmauZIeo0S8'
-export const FIREBASE_PROJECT_ID = 'open-signage-staging'
-export const FIREBASE_DATABASE_URL = 'https://open-signage-staging.firebaseio.com/'
