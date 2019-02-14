dd5baafcb82fd0cf3ddc625be7e007a7dd239b8d
Zachary Muir <zack@fntech.com>
2019-02-14 10:09:00 -0800

    created ocp-2019-fnsign template

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
         'ocp-ams-content': './src/entry-ocp-ams-content.js',
         'ocp-ams-signage': './src/entry-ocp-ams-signage.js',
         'ocp-ams-fnsign': './src/entry-ocp-ams-fnsign.js',
+		'ocp-ams-fnsign': './src/entry-ocp-2019-fnsign.js',
         'ocp-ams-fnsign-landscape': './src/entry-ocp-ams-fnsign-landscape.js',
         'oc5-oculus-signage': './src/entry-oc5-oculus-signage.js',
         'oc5-oculus-registration': './src/entry-oc5-oculus-registration.js',
