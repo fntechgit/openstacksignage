eb093376f98a1ceefa26d3bc541d44da4e79ca9a
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2019-08-14 16:15:36 -0300

    Fixes router import path.

--- src/entry-oc6.js
+++ src/entry-oc6.js
@@ -3,7 +3,7 @@ import App from './components/app.vue'
 
 import Schedule from './model/schedule'
 import { $store } from './store'
-import { $router } from './router/oc6'
+import { $router } from './router/router-oc6'
 
 require('./firebase')
 
