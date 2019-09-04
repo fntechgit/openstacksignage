4758e596954d0225ecb5e81bd8ff98d62635fdbf
Zachary Muir <zack@fntech.com>
2019-09-04 12:00:14 -0700

    created template ocp-ams-image-g001

---
+++ src/components/ocp-ams-image-g001/banner.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-bind:style="style">
+    </div>
+</template>
+
+<script>
+
+    export default {
+        props: ['banner'],
+        computed: {
+            style: function() {
+                return {
+                    'min-height': '100vh',
+                    'height': '1920px',
+                    'width' : '1080px', 
+                    'background-size': 'cover',
+                    'background': 'url(assets/images/MR1_G001_OCP19R_260919_0000-1230.png)',
+                };
+            }
+        },
+    }
+
+</script>>
