b33314b42c98d38760fc1fe0ed3ce2e18fb0353c
Zachary Muir <zack@fntech.com>
2019-09-05 07:10:58 -0700

    created template ocp-ams-image-g002

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,7 @@ module.exports = {
 		'ocp-ams-image': './src/entry-ocp-ams-image.js',
 		'ocp-ams-image-g001': './src/entry-ocp-ams-image-g001.js',
 		'ocp-ams-image-g001v2': './src/entry-ocp-ams-image-g001v2.js',
+		'ocp-ams-image-g001v2': './src/entry-ocp-ams-image-g002.js',		
 		'ocp-ams-image-landscape': './src/entry-ocp-ams-image-landscape.js',
         //'ocp-ams-meeting': './src/entry-ocp-ams-meeting.js',
         //'ocp-ams-content': './src/entry-ocp-ams-content.js',
