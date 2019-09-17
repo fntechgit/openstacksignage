f63108dd23c3fa9d88adf5dbc8358585adf9f69e
Zachary Muir <zack@fntech.com>
2019-09-17 13:29:53 -0700

    Edited oc6-oculus-signage-sp template

---
+++ src/components/oc6-oculus-sp/registration/events.vue
@@ -0,0 +1,185 @@
+<template>
+    <div class="event next pb-5">
+        <div class="pt-5">
+            <div class="container-fluid pl-5 pr-5">
+                <div class="events-wrapper">
+                    <swiper ref="slider" :options="swiperOption">
+                        <swiper-slide :key="0">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h1 class="text-primary name">
+                                        Registration
+                                    </h1>
+                                </div>
+                            </div>
+                            <div class="row pb-3">
+                                <div>
+                                    <h1 class="venue">
+                                        The Hub
+                                    </h1>
+                                </div>
+                                <div>
+                                    <h1 class="time">
+                                        08:00 AM - 09:00 PM
+                                    </h1>
+                                </div>
+                            </div>
+                        </swiper-slide>
+                        <swiper-slide v-for="event in events" :key="event.id">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h1 class="text-primary name">
+                                        {{ event.title }}
+                                    </h1>
+                                </div>
+                            </div>
+                            <div class="row pb-3">
+                                <div>
+                                    <h1 class="venue">
+                                        {{ getVenue(event) }}
+                                    </h1>
+                                </div>
+                                <div>
+                                    <h1 class="time">
+                                        {{ time(event) }}
+                                    </h1>
+                                </div>
+                            </div>
+                        </swiper-slide>
+                    </swiper>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+  import 'swiper/dist/css/swiper.css'
+
+  import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+  export default {
+    props: ['events', 'schedule'],
+    data() {
+      return {
+        swiperOption: {
+          direction: 'vertical',
+          allowTouchMove: false,
+          slidesPerView: 'auto',
+          height: 1,
+          spaceBetween: 10,
+          loop: true,
+          speed: 4000,
+          autoplay: {
+            delay: 0
+          }
+        }
+      }
+    },
+    beforeUpdate: function () {
+      this.swiper.detachEvents()
+      this.swiper.destroy()
+    },
+    updated: function () {
+      this.$refs.slider.mountInstance()
+    },
+    mounted: function () {
+    },
+    computed: {
+      swiper() {
+        return this.$refs.slider.swiper
+      },
+      time() {
+        return event => event && event.start_date && event.end_date && [
+          this.schedule.getDate(event.start_date).format('hh:mm A'),
+          this.schedule.getDate(event.end_date).format('hh:mm A')
+        ].join(' - ') || ' '
+      },
+      starttime() {
+        return event => event && this.schedule.getDate(event.start_date).format('HH:mm') || 'N/A'
+      },
+      getVenue() {
+        return event => event.location && event.location.name || ' '
+      }
+
+    },
+    components: {
+      swiper,
+      swiperSlide
+    }
+  }
+</script>
+
+<style>
+    .next .time {
+        font-size: 32px;
+        letter-spacing: 1px;
+        color: #fff;
+        font-family: "Oculus Sans";
+        text-rendering: geometricPrecision;
+        -webkit-font-smoothing: antialiased;
+    }
+
+    .next .venue {
+        font-size: 32px;
+        letter-spacing: 1px;
+        color: #148aff;
+        font-family: "Oculus Sans";
+        text-rendering: geometricPrecision;
+        -webkit-font-smoothing: antialiased;
+        padding-right: 15px;
+        padding-left: 17px;
+    }
+    .next .name {
+        font-family: Oculus Sans;
+        color: #fff!important;
+        text-rendering: geometricPrecision;
+        -webkit-font-smoothing: antialiased;
+    }
+    .next {
+        position: relative;
+        top: 200px;
+        width: 900px;
+        margin-left: 36px;
+        padding-bottom: 0px!important;
+    }
+    .next .upcoming {
+        font-size: 33px;
+        letter-spacing: 3px;
+        color: rgb(52,56,149);
+        font-family: "Nexa";
+        font-weight: bold;
+    }
+    .next .name {
+        font-size: 40px;
+        padding-bottom: 5px;
+        letter-spacing: 1px;
+    }
+    .next .speaker,
+    .next .speaker-info {
+        font-family: Franklin;
+        font-size: 34px;
+        text-rendering: geometricPrecision;
+        -webkit-font-smoothing: antialiased;
+    }
+    .next .speaker {
+        color: #fff;
+    }
+    .next .speaker-info {
+        color: rgb(52,56,149);
+    }
+    .events-wrapper {
+        height: 1550px;
+        overflow: hidden;
+    }
+    .swiper-wrapper {
+        -webkit-transition-timing-function: linear!important;
+        -o-transition-timing-function: linear!important;
+        transition-timing-function: linear!important;
+    }
+    div[data-swiper-slide-index="0"] h1,
+    div[data-swiper-slide-index="0"] h1.name {
+        color: black!important;
+    }
+</style>
---
+++ src/components/oc6-oculus-sp/signage/event.vue
@@ -0,0 +1,165 @@
+<template>
+    <div class="event pb-5" :class="{ next , fixed }">
+        <div class="pt-5">
+            <div class="container-fluid pl-5 pr-5">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="text-primary name">
+                            {{ event.title }}
+                        </h1>
+                    </div>
+                </div>
+                <div class="row" v-if="!next && event.speakers">
+                    <div class="col-md-12 speaker-list-label">
+                         <h1 class="text-uppercase">
+                            Speakers
+                        </h1>
+                    </div>
+	            <div class="col-md-12 speaker-list">
+                        <li v-for="speaker in event.speakers">
+                            <h1 class="speaker">
+                                {{ speakername(speaker) }}
+                            </h1>
+                            <h1 class="speaker-info">
+                                {{ speakerinfo(speaker) }}
+                            </h1>
+                        </li>
+    	            </div>
+                </div>
+                <div class="row pb-3">
+                    <div class="col-md-12">
+                        <h1 class="time">
+                            {{ time(event) }}
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['next', 'event', 'schedule', 'fixed'],
+        computed: {
+            room() {
+                return event => event && this.$store.getters.room(
+                    event.location_id
+                ) || { name: 'N/A' }
+            },
+            time() {
+                return event => event && [
+                    this.schedule.getDate(event.start_date).format('hh:mm A'),
+                    this.schedule.getDate(event.end_date).format('hh:mm A')
+                ].join(' - ') || 'N/A'
+            },
+            speakername() {
+                return speaker => speaker && [
+                    speaker.first_name,
+                    speaker.last_name
+                ].join(' ') || 'N/A'
+            },
+            speakerinfo() {
+                return speaker => speaker && (speaker.position && speaker.company) && [
+                    speaker.position,
+                    speaker.company
+                ].join(', ') 
+            },
+        }
+    }
+</script>
+
+<style>
+    .event {
+        width: 1000px;
+        margin-left: 36px;
+        position: relative;
+        top: 660px;
+        height: 700px;
+    }
+    .event .time,
+    .next .time {
+        font-size: 32px;
+        letter-spacing: 1px;
+        color: rgb(186,1,255);
+        font-family: "Oculus Sans";
+        font-weight: bold;
+        text-rendering: geometricPrecision;
+	-webkit-font-smoothing: antialiased;
+    }
+    .event .name,
+    .next .name {
+        font-family: Oculus Sans;
+        color: #fff!important;
+        text-rendering: geometricPrecision;
+        -webkit-font-smoothing: antialiased;
+    }
+    .event .name {
+        font-size: 50px;
+        letter-spacing: 1px;
+        padding-bottom: 10px;	
+    }
+    .event .speaker-list {
+        list-style: none;
+    }
+    .speaker-list li {
+        display:block;
+    }
+    .speaker-list-label h1{
+        font-size: 40px;
+        letter-spacing: 1px;
+        color: rgb(93,94,97);
+        font-family: Oculus Sans;
+        padding-bottom: 10px;
+    }
+    .event .speaker{
+        font-family: Oculus Sans;
+        font-size: 40px;
+        letter-spacing: 1px;
+        color: #fff;
+        text-rendering: geometricPrecision;
+        -webkit-font-smoothing: antialiased;
+    }
+    .event .speaker-info {
+        font-family: Oculus Sans;
+        font-size: 20px;
+        letter-spacing: 1px;
+        color: rgb(93,94,97);
+        text-rendering: geometricPrecision;
+        -webkit-font-smoothing: antialiased;
+
+    }
+    .next {
+        position: relative;
+        top: 700px;
+    }
+    .next .upcoming {
+        font-size: 33px;
+        letter-spacing: 3px;
+        color: rgb(52,56,149);
+        font-family: "Nexa";
+        font-weight: bold;
+    }
+    .next .name {
+        font-size: 50px;
+        padding-bottom: 5px;
+        letter-spacing: 1px;
+    }
+    .next .speaker,
+    .next .speaker-info {
+        font-family: Franklin;
+        font-size: 34px;
+        text-rendering: geometricPrecision;
+        -webkit-font-smoothing: antialiased;
+    }
+    .next .speaker {
+        color: #fff;
+    }
+    .next .speaker-info {
+        color: rgb(52,56,149);
+    }
+    .event.pb-5.next.fixed {
+        position: absolute!important;
+        top: 1520px;
+    }
+</style>
