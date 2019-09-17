fbd5a5438a9bd29a31fbeaf79b4b6c1e0ac09831
Zachary Muir <zack@fntech.com>
2019-09-17 14:37:27 -0700

    Fixes for OC6 templates

--- src/components/oc6-oculus-thu/registration/screen.vue
+++ src/components/oc6-oculus-thu/registration/screen.vue
@@ -193,13 +193,13 @@
     #app.day-first {
         width: 1080px;
         height: 1920px;
-        background-image: url("~/assets/images/oc6-oculus/OC6_FNSIGN_Bkgd_LMU_edit.png");
+        background-image: url("~/assets/images/oc6-oculus/OC6_FNSIGN_Bkgd-thu.png");
     }    
 
     #app.day-second {
         width: 1080px;
         height: 1920px;
-        background-image: url("~/assets/images/oc6-oculus/OC6_FNSIGN_Bkgd_LMU_edit.png");
+        background-image: url("~/assets/images/oc6-oculus/OC6_FNSIGN_Bkgd-thu.png");
     }
 
 </style>
