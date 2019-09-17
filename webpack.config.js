2328a35470a27e699c7da9ffe6e35ba3b1fe4d4e
Zachary Muir <zack@fntech.com>
2019-09-17 14:16:33 -0700

    Added oc6-oculus-registration-thu template

--- webpack.config.js
+++ webpack.config.js
@@ -57,6 +57,7 @@ module.exports = {
         'oc6-oculus-signage-tt': './src/entry-oc6-oculus-signage-tt.js',
         'oc6-oculus-signage-sp': './src/entry-oc6-oculus-signage-sp.js',
         'oc6-oculus-registration': './src/entry-oc6-oculus-registration.js',
+        'oc6-oculus-registration-thu': './src/entry-oc6-oculus-registration-thu.js',
         'oc6-oculus-registration-wed': './src/entry-oc6-oculus-registration-wed.js',
         //'my-new-template': './src/entry-my-new-template.js',
 		//'ocp-2019-sj-d1-v1': './src/entry-ocp-2019-sj-d1-v1.js',
