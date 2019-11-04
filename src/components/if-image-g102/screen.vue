d321abed5bd335a1143021fc1c518b17398c657d
Zachary Muir <zack@fntech.com>
2019-11-04 14:38:22 -0800

    Created static image templates for if

---
+++ src/components/if-image-g102/screen.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <banner :banner="banner.state.static_banner"></banner>
+    </div>
+</template>
+
+<script>
+
+    import 'assets/css/theme.scss'
+    
+    import Banner from './banner.vue'
+    import { mapGetters } from 'vuex'
+
+    export default {
+        computed: {
+            ...mapGetters({
+                banner: 'banner'
+            }),
+        },
+        components: { Banner }
+    }
+
+</script>
---
+++ src/router/router-if-image-g101.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Screen from '../components/if-image-g101/screen.vue'
+
+Vue.use(VueRouter)
+
+export const $router = new VueRouter({ routes: [
+    { path: '*', name: 'home', component: Screen }
+]})
