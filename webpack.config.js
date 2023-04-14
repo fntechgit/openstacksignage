3a8d74896aa7033fdea100f5491df655dcb79c0b
Tomás Castillo <tcastilloboireau@gmail.com>
2023-04-14 17:19:36 -0300

    Webpack changes for entrypoints, add template folder structure
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const fs = require('fs')
 const webpack = require('webpack')
 const dotenv = require('dotenv')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
@@ -16,76 +17,58 @@ const env = config.parsed,
         return prev;
     }, {});
 
-const entry = {
-    'config-admin': './src/config-admin.js',
-    'image': './src/entrypoints/entry-image.js',
-    'image-landscape': './src/entrypoints/entry-image-landscape.js',
-    //'banner': './src/entrypoints/entry-banner.js',
-    'schedule': './src/entrypoints/entry-schedule.js',
-    'ocp-2022': './src/entrypoints/entry-ocp-2022',
-    'ocp-2022-expo-hall-talks': './src/entrypoints/entry-ocp-2022-expo-hall-talks',
-    'ocp-2022-session-full': './src/entrypoints/entry-ocp-2022-session-full',
-    'ocp-2022-empowering-open': './src/entrypoints/entry-ocp-2022-empowering-open',
-    /*
-    'ocp-2021': './src/entrypoints/entry-ocp-2021',
-    'ocp-2021-expo-hall-talks': './src/entrypoints/entry-ocp-2021-expo-hall-talks',
-    'ocp-2021-lightning-talks': './src/entrypoints/entry-ocp-2021-lightning-talks',
-    'ocp-2021-session-full': './src/entrypoints/entry-ocp-2021-session-full',
-    'ocp-2021-open-possibilities': './src/entrypoints/entry-ocp-2021-open-possibilities',
-    'infinity-schedule': './src/entrypoints/entry-infinity-schedule.js',
-    'infinity-sessions': './src/entrypoints/entry-infinity-sessions.js',
-    'if-image-d101': './src/entrypoints/entry-if-image-d101.js',
-    'if-image-d102': './src/entrypoints/entry-if-image-d102.js',
-    'if-image-g101': './src/entrypoints/entry-if-image-g101.js',
-    'if-image-g102': './src/entrypoints/entry-if-image-g102.js',
-    'if-image-od01': './src/entrypoints/entry-if-image-od01.js',
-    'if-image-od02': './src/entrypoints/entry-if-image-od02.js',
-    'if-image-od03': './src/entrypoints/entry-if-image-od03.js',
-    'if-image-od04': './src/entrypoints/entry-if-image-od04.js',
-    'ocp-2020': './src/entrypoints/entry-ocp-2020',
-    'ocp-2020-expo-hall-talks': './src/entrypoints/entry-ocp-2020-expo-hall-talks',
-    'ocp-2020-lightning-talks': './src/entrypoints/entry-ocp-2020-lightning-talks',     
-    'schedule-mint': './src/entrypoints/entry-schedule-mint.js',
-    'ocp-2019-breakout-rooms': './src/entrypoints/entry-ocp-2019-breakout-rooms',
-    'ocp-2019-expo-hall-talks': './src/entrypoints/entry-ocp-2019-expo-hall-talks',
-    'ocp-2019-ams-exec-tracks-portrait': './src/entrypoints/entry-ocp-2019-ams-exec-tracks-portrait',
-    'ocp-2019-ams-exec-tracks-landscape': './src/entrypoints/entry-ocp-2019-ams-exec-tracks-landscape',
-    'ocp-ams-meeting': './src/entrypoints/entry-ocp-ams-meeting.js',
-    'ocp-ams-content': './src/entrypoints/entry-ocp-ams-content.js',
-    'ocp-ams-signage': './src/entrypoints/entry-ocp-ams-signage.js',
-    'ocp-ams-fnsign': './src/entrypoints/entry-ocp-ams-fnsign.js',
-    'ocp-ams-fnsign-landscape': './src/entrypoints/entry-ocp-ams-fnsign-landscape.js',
-    'ocp-ams-image': './src/entrypoints/entry-ocp-ams-image.js',
-    'ocp-ams-image-landscape': './src/entrypoints/entry-ocp-ams-image-landscape.js',
-    'ocp-ams-image-g001': './src/entrypoints/entry-ocp-ams-image-g001.js',
-    'ocp-ams-image-g001v2': './src/entrypoints/entry-ocp-ams-image-g001v2.js',
-    'ocp-ams-image-g002': './src/entrypoints/entry-ocp-ams-image-g002.js',  
-    'ocp-ams-image-g003': './src/entrypoints/entry-ocp-ams-image-g003.js',      
-    'ocp-ams-image-g101': './src/entrypoints/entry-ocp-ams-image-g101.js',      
-    'ocp-ams-image-g108': './src/entrypoints/entry-ocp-ams-image-g108.js',
-    'ocp-ams-image-g109': './src/entrypoints/entry-ocp-ams-image-g109.js',
-    'ocp-ams-image-g110': './src/entrypoints/entry-ocp-ams-image-g110.js',
-    'ocp-ams-image-g111': './src/entrypoints/entry-ocp-ams-image-g111.js',
-    'oculus-oc5-signage': './src/entrypoints/entry-oculus-oc5-signage.js',
-    'oculus-oc5-registration': './src/entrypoints/entry-oculus-oc5-registration.js',
-    'oc6-oculus-signage': './src/entrypoints/entry-oc6-oculus-signage.js',
-    'oc6-oculus-signage-220b': './src/entrypoints/entry-oc6-oculus-signage-220b.js',
-    'oc6-oculus-signage-220c': './src/entrypoints/entry-oc6-oculus-signage-220c.js',
-    'oc6-oculus-signage-210f': './src/entrypoints/entry-oc6-oculus-signage-210f.js',
-    'oc6-oculus-signage-210h': './src/entrypoints/entry-oc6-oculus-signage-210h.js',
-    'oc6-oculus-signage-tt': './src/entrypoints/entry-oc6-oculus-signage-tt.js',
-    'oc6-oculus-signage-sp': './src/entrypoints/entry-oc6-oculus-signage-sp.js',
-    'oculus-oc6-sessions': './src/entrypoints/entry-oculus-oc6-sessions.js',
-    'oculus-oc6-schedule': './src/entrypoints/entry-oculus-oc6-schedule.js',
-    'oc6-oculus-image-eod1': './src/entrypoints/entry-oc6-oculus-image-eod1.js',
-    'oc6-oculus-image-eod2': './src/entrypoints/entry-oc6-oculus-image-eod2.js',
-    'oc6-oculus-image-sessionfull': './src/entrypoints/entry-oc6-oculus-image-sessionfull.js',
-    'f8-schedule': './src/entrypoints/entry-f8-schedule.js',
-    'f8-sessions': './src/entrypoints/entry-f8-sessions.js',*/
+const getHTMLFiles = (dir, fileList = []) => {
+    const files = fs.readdirSync(dir);
+    files.forEach((file) => {
+        const filePath = path.join(dir, file);
+        const isDirectory = fs.statSync(filePath).isDirectory();
+
+        if (isDirectory) {
+            getHTMLFiles(filePath, fileList);
+        } else {
+            if (path.extname(filePath).toLowerCase() === '.html') {
+                const relativePath = path.relative('templates', filePath);
+                fileList.push(relativePath);
+            }
+        }
+
+    });
+    return fileList;
+};
+
+const htmlTemplatesFiles = getHTMLFiles('./templates');
+
+const generateTemplateEntryPoints = (templateList) => {
+
+    const defaultEntryPoints = {
+        'config-admin': './src/config-admin.js',
+        'image-landscape': './src/entrypoints/entry-image-landscape.js',
+        'image': './src/entrypoints/entry-image.js',
+        'banner': './src/entrypoints/entry-banner.js',
+        'schedule': './src/entrypoints/entry-schedule.js',
+    };
+
+    const entryPoints = {};
+
+    templateList.forEach((templatePath) => {
+        const summitIdRegex = /\/(\d+)\//;
+        const entryNameRegex = /\/([\w-]+)\.html$/;
+        const summitIdMatch = templatePath.match(summitIdRegex);
+        const entryNameMatch = templatePath.match(entryNameRegex);
+
+        if (summitIdMatch && entryNameMatch) {
+            const entryName = entryNameMatch[1];
+            entryPoints[entryName] = `./src/entrypoints/entry-${entryName}`;
+        }
+    });
+
+    return { ...defaultEntryPoints, ...entryPoints };
 }
 
+const entrypoints = generateTemplateEntryPoints(htmlTemplatesFiles);
+
 module.exports = {
-    entry: entry,
+    entry: entrypoints,
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].build.js',
@@ -170,10 +153,15 @@ const entryHtmlPlugins = Object.keys(entry).map(function(entryName) {
         inject = 'head'
         fileName = 'admin'
     }
+    const templateFile = htmlTemplatesFiles.find((file) => {
+        // Check if the file name matches the entry name
+        const name = file.split('/')[2].split('.')[0];
+        return name === entryName;
+    });
     return new HtmlWebpackPlugin({
         inject: inject,
         hash: true,
-        template: `${fileName}.html`,
+        template: `${templateFile ? `templates/${templateFile}` : `${fileName}.html`}`,
         filename: `${fileName}.html`,
         chunks: chunks
     })
