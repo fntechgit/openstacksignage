37a383926fc43f4d9ed32af6ccb24f6695bd5a5b
smarcet@gmail.com <smarcet@gmail.com>
2023-06-14 20:09:05 -0500

    Fix on data updates
    * fixed bug on event synch strategy
    
    Signed-off-by: smarcet@gmail.com <smarcet@gmail.com>

--- src/workers/sync_strategies/activity_synch_strategy.js
+++ src/workers/sync_strategies/activity_synch_strategy.js
@@ -8,8 +8,8 @@ import {insertSorted, intCheck} from "../../utils/array_utils";
 class ActivitySynchStrategy extends AbstractSynchStrategy{
 
     async process(payload){
-
-        console.log(`ActivitySynchStrategy::process`, payload);
+        const locationId = this.location;
+        console.log(`ActivitySynchStrategy::process locationId ${locationId}`, payload);
 
         const {entity_operator, entity_id} = payload;
 
@@ -33,11 +33,13 @@ class ActivitySynchStrategy extends AbstractSynchStrategy{
             }
             else {
                 // entity is published
-
+                // try to get it from current data-set
                 let idx = this.allIDXEvents.hasOwnProperty(entity.id) ? this.allIDXEvents[entity.id] : -1;
                 console.log(`ActivitySynchStrategy::process entity is published got idx ${idx} eventsData length ${eventsData.length}`);
                 let formerEntity = idx === -1 ? null : ( (eventsData.length - 1 ) >= idx ? eventsData[idx] : null ) ;
+
                 console.log(`ActivitySynchStrategy::process entity is published`, formerEntity, entity, idx);
+
                 if (formerEntity && formerEntity.id !== entity.id) {
                     console.log('ActivitySynchStrategy::process entity is published entities are not the same. looking on events');// it's not the same
                     formerEntity = eventsData.find((e, index) => {
@@ -52,8 +54,15 @@ class ActivitySynchStrategy extends AbstractSynchStrategy{
                         return Promise.reject(`ActivitySynchStrategy::process entity id ${entity.id} not found`);
                     }
                 }
+
                 if(!formerEntity){
-                    console.log('ActivitySynchStrategy::process former entity does not exists, inserting new one', entity);
+                    console.log('ActivitySynchStrategy::process former entity does not exists, trying to inserting new one', entity);
+                    // entity didnt exists on current data set , check if belongs to this location ...
+                    const currentEventLocationId = entity.hasOwnProperty('location')? entity.location.id:entity.location_id;
+                    if(currentEventLocationId != locationId){
+                        return Promise.reject(`ActivitySynchStrategy::process entity id ${entity.id} belongs to another location ${currentEventLocationId} and we are on location ${locationId}`);
+                    }
+
                     // entity was just published ... then do insert ordering
                     this.allIDXEvents[entity.id] = insertSorted(eventsData, entity, (a, b) => {
                         // multi-criteria sort
@@ -75,7 +84,8 @@ class ActivitySynchStrategy extends AbstractSynchStrategy{
                     // check location
                     const formerLocationId = formerEntity.location.id;
                     const currentLocationId = entity.hasOwnProperty('location')? entity.location.id:entity.location_id;
-                    if(formerLocationId != currentLocationId){
+
+                    if(formerLocationId != currentLocationId || currentLocationId != locationId){
                         console.log(`ActivitySynchStrategy::process non matching locations, former location ${formerLocationId} current location ${currentLocationId} for ${entity.id} at idx ${idx}`)
                         // we need to remove it
                         if(idx === -1)
