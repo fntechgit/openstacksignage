1be73f56caacaabc3ea52f62fcd1af6005e40d82
Tomás Castillo <tomrndom@users.noreply.github.com>
2023-10-05 15:37:38 -0300

    WIP - new files for ocp2023 (#4)
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

---
+++ src/components/fntech/ocp/2023/global/session-full/screen.vue
@@ -0,0 +1,101 @@
+<template>
+    <div id="app">
+
+        <table v-if="schedule.debug" border="1" width="100%" class="debug">
+            <tr>
+                <td align="center" colspan="3" v-html="schedule.format(schedule.state.now)"></td>
+            </tr>
+            <tr>
+                <td align="center" width="33%">
+                    <a v-if="schedule.state.events.prev" href="" @click.prevent="syncStart(schedule.state.events.prev)">[start-5s]</a>
+                    Previous Event
+                    <a v-if="schedule.state.events.prev" href="" @click.prevent="syncEnd(schedule.state.events.prev)">[end-5s]</a>
+                </td>
+                <td align="center" width="33%">
+                    <a v-if="schedule.state.events.curr" href="" @click.prevent="syncStart(schedule.state.events.curr)">[start-5s]</a>
+                    Current Event
+                    <a v-if="schedule.state.events.curr" href="" @click.prevent="syncEnd(schedule.state.events.curr)">[end-5s]</a>
+                </td>
+                <td align="center" width="33%">
+                    <a v-if="schedule.state.events.next" href="" @click.prevent="syncStart(schedule.state.events.next)">[start-5s]</a>
+                    Next Event
+                    <a v-if="schedule.state.events.next" href="" @click.prevent="syncEnd(schedule.state.events.next)">[end-5s]</a>
+                </td>
+            </tr>
+            <tr>
+                <td align="center" width="33%">
+                    {{ schedule.state.events.prev && schedule.state.events.prev.title || 'N/A' }}
+                </td>
+                <td align="center" width="33%">
+                    {{ schedule.state.events.curr && schedule.state.events.curr.title || 'N/A' }}
+                </td>
+                <td align="center" width="33%">
+                    {{ schedule.state.events.next && schedule.state.events.next.title || 'N/A' }}
+                </td>
+            </tr>
+            <tr>
+                <td align="center" width="33%">
+                    <a v-if="schedule.state.scheduled_banners.prev" href="" @click.prevent="syncStart(schedule.state.scheduled_banners.prev)">[start-5s]</a>
+                    Previous Banner
+                    <a v-if="schedule.state.scheduled_banners.prev" href="" @click.prevent="syncEnd(schedule.state.scheduled_banners.prev)">[end-5s]</a>
+                </td>
+                <td align="center" width="33%">
+                    <a v-if="schedule.state.scheduled_banners.curr" href="" @click.prevent="syncStart(schedule.state.scheduled_banners.curr)">[start-5s]</a>
+                    Current Banner
+                    <a v-if="schedule.state.scheduled_banners.curr" href="" @click.prevent="syncEnd(schedule.state.scheduled_banners.curr)">[end-5s]</a>
+                </td>
+                <td align="center" width="33%">
+                    <a v-if="schedule.state.scheduled_banners.next" href="" @click.prevent="syncStart(schedule.state.scheduled_banners.next)">[start-5s]</a>
+                    Next Banner
+                    <a v-if="schedule.state.scheduled_banners.next" href="" @click.prevent="syncEnd(schedule.state.scheduled_banners.next)">[end-5s]</a>
+                </td>
+            </tr>
+            <tr>
+                <td align="center" width="33%">
+                    {{ schedule.state.scheduled_banners.prev && schedule.state.scheduled_banners.prev.title || 'N/A' }}
+                </td>
+                <td align="center" width="33%">
+                    {{ schedule.state.scheduled_banners.curr && schedule.state.scheduled_banners.curr.title || 'N/A' }}
+                </td>
+                <td align="center" width="33%">
+                    {{ schedule.state.scheduled_banners.next && schedule.state.scheduled_banners.next.title || 'N/A' }}
+                </td>
+            </tr>
+            <tr>
+                <td align="center" colspan="3">
+                    Static Banner: {{ schedule.state.static_banner && schedule.state.static_banner.content || 'N/A' }}
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+
+    import 'assets/css/ocp/2019/global/theme.scss'
+
+    import moment from 'moment'
+
+    import { mapGetters } from 'vuex'
+
+    export default {
+        computed: {
+            ...mapGetters({
+                schedule: 'schedule'
+            }),
+        },
+        methods: {
+            syncStart(item) {
+                this.schedule.setOffset(
+                    item.start_date - moment.utc().unix() - 65
+                )
+            },
+            syncEnd(item) {
+                this.schedule.setOffset(
+                    item.end_date - moment.utc().unix() - 5
+                )
+            },
+        }
+    }
+
+</script>
