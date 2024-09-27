9f4e1a4ce36ce922e5aa9a25c15c708434539159
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2024-09-27 16:20:01 -0300

    Adds OCP 2024 signage templates.

---
+++ src/router/router-ocp-2024-expo-hall-talks.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueQRCodeComponent from 'vue-qrcode-component'
+
+import Screen from '../components/fntech/ocp/2024/global/expo-hall-talks/screen.vue'
+
+Vue.use(VueRouter)
+Vue.component('qr-code', VueQRCodeComponent)
+
+export const $router = new VueRouter({ routes: [
+    { path: '*', name: 'home', component: Screen }
+]})
