f6f1e1952023a97540273f79661d524bb595538d
smarcet <smarcet@gmail.com>
2025-08-16 15:18:50 -0300

    chore: refactor ActivitySynchStrategy
    this strategy was overwriting the overflow_url set by another strategy
    breaking the stream overflow mechanism

--- src/workers/sync_strategies/activity_synch_strategy.js
+++ src/workers/sync_strategies/activity_synch_strategy.js
@@ -1,6 +1,23 @@
 import AbstractSynchStrategy from "./abstract_synch_strategy";
 import {fetchEventById} from "../actions/fetch-entities-actions";
-import {insertSorted, intCheck} from "../../utils/array_utils";
+import {insertSorted, intCheck, rebuildIndex} from "../../utils/array_utils";
+
+// Location extractor (supports location.id or location_id)
+const getEventLocationId = (e) => e?.location?.id ?? e?.location_id ?? null;
+
+// Keep local/transient fields set by other strategies (e.g., overflow_url)
+const PRESERVE_FIELDS = ["overflow_url"];
+
+const mergeTransientFields = (former, next) => {
+    if (!former) return next;
+    const merged = { ...next };
+    for (const f of PRESERVE_FIELDS) {
+        if (former[f] !== undefined && merged[f] === undefined) {
+            merged[f] = former[f];
+        }
+    }
+    return merged;
+};
 
 /**
  * ActivitySynchStrategy
@@ -16,135 +33,59 @@ class ActivitySynchStrategy extends AbstractSynchStrategy{
         const entity = await fetchEventById(this.summit.id, entity_id, this.accessToken);
         let eventsData = [...this.allEvents];
 
-        if (entity_operator === 'UPDATE') {
-
-            if(!entity){
-                // was deleted ( un - published)
-                // try to get from index
-                const idx =  this.allIDXEvents.hasOwnProperty(entity_id) ? this.allIDXEvents[entity_id] : -1;
-                console.log(`ActivitySynchStrategy::process unpublished presentation ${entity_id} idx ${idx}`);
-
-                if(idx === -1)
-                    return Promise.reject('ActivitySynchStrategy::process unpublished idx === -1.'); // does not exists on index ...
-                // remove it from dataset
-                eventsData.splice(idx, 1);
-                // remove it from index
-                delete this.allIDXEvents[entity_id];
-            }
-            else {
-                // entity is published
-                // try to get it from current data-set
-                let idx = this.allIDXEvents.hasOwnProperty(entity.id) ? this.allIDXEvents[entity.id] : -1;
-                console.log(`ActivitySynchStrategy::process entity is published got idx ${idx} eventsData length ${eventsData.length}`);
-                let formerEntity = idx === -1 ? null : ( (eventsData.length - 1 ) >= idx ? eventsData[idx] : null ) ;
-
-                console.log(`ActivitySynchStrategy::process entity is published`, formerEntity, entity, idx);
-
-                if (formerEntity && formerEntity.id !== entity.id) {
-                    console.log('ActivitySynchStrategy::process entity is published entities are not the same. looking on events');// it's not the same
-                    formerEntity = eventsData.find((e, index) => {
-                        let res = e.id == entity.id;
-                        if(res){
-                            console.log(`ActivitySynchStrategy::process entity id ${entity.id} found at idx ${index}`);
-                            idx = index;
-                        }
-                        return res;
-                    });
-                    if(!formerEntity){
-                        return Promise.reject(`ActivitySynchStrategy::process entity id ${entity.id} not found`);
-                    }
-                }
-
-                if(!formerEntity){
-                    console.log('ActivitySynchStrategy::process former entity does not exists, trying to inserting new one', entity);
-                    // entity didnt exists on current data set , check if belongs to this location ...
-                    const currentEventLocationId = entity.hasOwnProperty('location')? entity.location.id:entity.location_id;
-                    if(currentEventLocationId != locationId){
-                        return Promise.reject(`ActivitySynchStrategy::process entity id ${entity.id} belongs to another location ${currentEventLocationId} and we are on location ${locationId}`);
-                    }
-
-                    // entity was just published ... then do insert ordering
-                    this.allIDXEvents[entity.id] = insertSorted(eventsData, entity, (a, b) => {
-                        // multi-criteria sort
-
-                        if (a.start_date === b.start_date) {
-
-                            if (a.end_date === b.end_date) {
-                                return intCheck(a.id, b.id);
-                            }
-
-                            return intCheck(a.end_date, b.end_date);
-                        }
-
-                        return intCheck(a.start_date, b.start_date);
-                    });
-                }
-                else{
-                    // we have a former entity
-                    // check location
-                    const formerLocationId = formerEntity.location.id;
-                    const currentLocationId = entity.hasOwnProperty('location')? entity.location.id:entity.location_id;
-
-                    if(formerLocationId != currentLocationId || currentLocationId != locationId){
-                        console.log(`ActivitySynchStrategy::process non matching locations, former location ${formerLocationId} current location ${currentLocationId} for ${entity.id} at idx ${idx}`)
-                        // we need to remove it
-                        if(idx === -1)
-                            return Promise.reject('ActivitySynchStrategy::process non matching location idx === -1.'); // does not exists on index ...
-                        // remove it from dataset
-                        eventsData.splice(idx, 1);
-                        // remove it from index
-                        delete this.allIDXEvents[entity_id];
-                    }
-                    else if
-                    (
-                        formerEntity.start_date === entity.start_date &&
-                        formerEntity.end_date === entity.end_date
-                    ) {
-                        // presentation was just updated
-                        console.log(`ActivitySynchStrategy::process updating presentation ${entity.id} at idx ${idx}`)
-                        eventsData[idx] = entity;
-                    } else {
-                        // publishing dates changed, we need to remove and do ordered re-insert
-                        // remove it first
-
-                        console.log(`ActivitySynchStrategy::process publishing dates had changed, deleting at idx ${idx}`, entity);
-                        eventsData.splice(idx, 1);
-                        // then do insert ordering
-                        this.allIDXEvents[entity.id] = insertSorted(eventsData, entity, (a, b) => {
-                            // multi-criteria sort
-
-                            if (a.start_date === b.start_date) {
-
-                                if (a.end_date === b.end_date) {
-                                    return intCheck(a.id, b.id);
-                                }
-
-                                return intCheck(a.end_date, b.end_date);
-                            }
-
-                            return intCheck(a.start_date, b.start_date);
-                        });
-
-                        let newAllIDXEvents = {};
-                        eventsData.forEach((e, index) => newAllIDXEvents[e.id] = index);
-                        this.allIDXEvents = newAllIDXEvents;
-                    }
-                }
-
-            }
-
-            let res = {
-                payload,
-                entity,
-                summit : this.summit,
-                eventsData,
-                allIDXEvents : this.allIDXEvents,
-            };
-
-            console.log(`ActivitySynchStrategy::process done`, res);
-
-            return Promise.resolve(res);
+        if (entity_operator !== "UPDATE") {
+            return Promise.reject("ActivitySynchStrategy::process not valid operator");
         }
+
+        if (!entity) {
+            // unpublished: remove
+            console.log(`ActivitySynchStrategy::process entity ${entity_id} not found at api removing it from local.`);
+            eventsData = eventsData.filter((e) => e.id !== entity_id);
+            this.allIDXEvents = rebuildIndex(eventsData);
+        }
+        else {
+
+            // locate former entity (to preserve overflow_url, etc.)
+            const formerIdx = this.allIDXEvents[entity.id] ?? eventsData.findIndex((e) => e.id === entity.id);
+            const formerEntity = formerIdx >= 0 ? eventsData[formerIdx] : null;
+            console.log(`ActivitySynchStrategy::process formerIdx ${formerIdx} formerEntity `, formerEntity);
+            const currentLocationId = getEventLocationId(entity);
+            if(currentLocationId !== locationId){
+                console.log(`ActivitySynchStrategy::process currentLocationId ${currentLocationId} <> locationId ${locationId}`);
+                // unpublished: remove
+                eventsData = eventsData.filter((e) => e.id !== entity_id);
+                this.allIDXEvents = rebuildIndex(eventsData);
+            }
+            else{
+
+                // preserve transient fields from the former entity (e.g., overflow_url)
+                const entityWithTransient = mergeTransientFields(formerEntity, entity);
+                console.log(`ActivitySynchStrategy::process re inserting with order entity `, entityWithTransient);
+                // ordered upsert: remove existing, then insert sorted
+                eventsData = eventsData.filter((e) => e.id !== entity.id);
+                this.allIDXEvents[entity.id] = insertSorted(eventsData, entityWithTransient, (a, b) => {
+                    if (a.start_date === b.start_date) {
+                        if (a.end_date === b.end_date) {
+                            return intCheck(a.id, b.id);
+                        }
+                        return intCheck(a.end_date, b.end_date);
+                    }
+                    return intCheck(a.start_date, b.start_date);
+                });
+                this.allIDXEvents = rebuildIndex(eventsData);
+            }
+        }
+
+        let res = {
+            payload,
+            entity,
+            summit : this.summit,
+            eventsData,
+            allIDXEvents : this.allIDXEvents,
+        };
+
+        console.log(`ActivitySynchStrategy::process done`, res);
+        return Promise.resolve(res);
     }
 }
 
